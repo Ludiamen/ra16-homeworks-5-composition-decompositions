11b15ca68bae22ea8a2925bef7a1b0418e4fab81
Ludiamen <maxvas1001@gmail.com>
2024-09-19 14:07:48 +0300

    initial commit

---
+++ src/components/Advertisement/Advertisement.tsx
@@ -0,0 +1,11 @@
+/**
+ * Компонент Advertisement: отвечает за рекламу.
+ */
+
+export const Advertisement: React.FC = () => {
+    return (
+      <div className="advertisement">
+        <img src="https://avatars.mds.yandex.net/i?id=d73fa28fbd6809fd8446a62ccb4e408c_l-5270141-images-thumbs&n=13" alt="Пример рекламы" />
+      </div>
+    );
+  };
